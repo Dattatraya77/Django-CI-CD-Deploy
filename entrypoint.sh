009df07ffdc76eeace383338272d4a92471d1491
Dattatraya Walunj <drwalunj.2010@gmail.com>
2026-04-10 18:20:10 +0530

    Added auto create superuser

---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo "Waiting for PostgreSQL..."
+
+while ! nc -z db 5432; do
+  sleep 1
+done
+
+echo "PostgreSQL Started"
+
+echo "Running Migrations"
+python manage.py migrate
+
+echo "Creating Superuser"
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = "Dattatraya77"
+email = "admin@gmail.com"
+password = "Mungale@77"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser Created")
+else:
+    print("Superuser Already Exists")
+END
+
+echo "Collect Static Files"
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn"
+gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
